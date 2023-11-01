3ad2fe1448bcf4bcf5431f285919688dbe9df15b
admin <phentalex@gmail.com>
2023-11-01 13:10:01 +0300

    Создано подключение к БД в Form1; Во второй создан вывод всех элементов из таблицы КА; Пытаюсь сделать подключение к БД для всех форм.

---
+++ Preform DB/Form2.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Preform_DB
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private DataTable table;
+
+        private SqlDataAdapter adapter = new SqlDataAdapter();
+
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            table.Clear();
+
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        Form1 f1 = new Form1();
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+            adapter = new SqlDataAdapter("SELECT * FROM KA", );
+            
+            table = new DataTable();
+            
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+    }
+}
