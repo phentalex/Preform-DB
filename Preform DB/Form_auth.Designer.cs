ba71caa1b274aee72b4926a822cbda3b6167211d
Александр Уваров <phentalex@gmail.com>
2023-11-19 15:04:41 +0300

    Удалил самую первую форму

--- Preform DB/Form_auth.Designer.cs
+++ Preform DB/Form_auth.Designer.cs
@@ -50,9 +50,10 @@
             // 
             this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(503, 258);
+            this.pictureBox4.Location = new System.Drawing.Point(1137, 569);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(34, 35);
+            this.pictureBox4.Size = new System.Drawing.Size(62, 65);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 9;
             this.pictureBox4.TabStop = false;
@@ -64,20 +65,22 @@
             // 
             this.textBox_log.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBox_log.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.textBox_log.Location = new System.Drawing.Point(293, 175);
+            this.textBox_log.Location = new System.Drawing.Point(752, 416);
+            this.textBox_log.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox_log.Multiline = true;
             this.textBox_log.Name = "textBox_log";
-            this.textBox_log.Size = new System.Drawing.Size(204, 35);
+            this.textBox_log.Size = new System.Drawing.Size(371, 61);
             this.textBox_log.TabIndex = 0;
             // 
             // textBox_pwd
             // 
             this.textBox_pwd.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBox_pwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.textBox_pwd.Location = new System.Drawing.Point(293, 258);
+            this.textBox_pwd.Location = new System.Drawing.Point(752, 569);
+            this.textBox_pwd.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox_pwd.Multiline = true;
             this.textBox_pwd.Name = "textBox_pwd";
-            this.textBox_pwd.Size = new System.Drawing.Size(204, 35);
+            this.textBox_pwd.Size = new System.Drawing.Size(371, 61);
             this.textBox_pwd.TabIndex = 1;
             // 
             // label1
@@ -85,9 +88,10 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
-            this.label1.Location = new System.Drawing.Point(252, 56);
+            this.label1.Location = new System.Drawing.Point(677, 196);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(284, 51);
+            this.label1.Size = new System.Drawing.Size(493, 85);
             this.label1.TabIndex = 2;
             this.label1.Text = "Авторизация";
             // 
@@ -96,9 +100,10 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label2.Location = new System.Drawing.Point(290, 145);
+            this.label2.Location = new System.Drawing.Point(747, 361);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 26);
+            this.label2.Size = new System.Drawing.Size(124, 44);
             this.label2.TabIndex = 3;
             this.label2.Text = "Логин";
             // 
@@ -107,9 +112,10 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label3.Location = new System.Drawing.Point(290, 229);
+            this.label3.Location = new System.Drawing.Point(747, 516);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 26);
+            this.label3.Size = new System.Drawing.Size(151, 44);
             this.label3.TabIndex = 4;
             this.label3.Text = "Пароль";
             // 
@@ -117,9 +123,10 @@
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.Location = new System.Drawing.Point(295, 299);
+            this.button1.Location = new System.Drawing.Point(756, 645);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(202, 52);
+            this.button1.Size = new System.Drawing.Size(370, 96);
             this.button1.TabIndex = 5;
             this.button1.Text = "Войти";
             this.button1.UseVisualStyleBackColor = true;
@@ -131,9 +138,10 @@
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(248, 171);
+            this.pictureBox1.Location = new System.Drawing.Point(670, 409);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(36, 39);
+            this.pictureBox1.Size = new System.Drawing.Size(66, 72);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -142,9 +150,10 @@
             // 
             this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(251, 254);
+            this.pictureBox2.Location = new System.Drawing.Point(675, 562);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(36, 39);
+            this.pictureBox2.Size = new System.Drawing.Size(66, 72);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
@@ -153,9 +162,10 @@
             // 
             this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(502, 258);
+            this.pictureBox3.Location = new System.Drawing.Point(1135, 569);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(34, 35);
+            this.pictureBox3.Size = new System.Drawing.Size(62, 65);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 8;
             this.pictureBox3.TabStop = false;
@@ -169,9 +179,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label4.Location = new System.Drawing.Point(343, 363);
+            this.label4.Location = new System.Drawing.Point(844, 763);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.Size = new System.Drawing.Size(190, 25);
             this.label4.TabIndex = 10;
             this.label4.Text = "Ещё нет аккаунта?";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -180,9 +191,9 @@
             // 
             // Form_auth
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1896, 1016);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
@@ -194,6 +205,7 @@
             this.Controls.Add(this.textBox_pwd);
             this.Controls.Add(this.textBox_log);
             this.Controls.Add(this.pictureBox4);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form_auth";
             this.Text = "Form_auth";
             this.Load += new System.EventHandler(this.Form_auth_Load);
